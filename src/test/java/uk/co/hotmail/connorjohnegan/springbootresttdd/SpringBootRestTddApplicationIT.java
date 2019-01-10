d94907417efc0fb371f8553f71a0a8548c2415dd
Connor Egan <connorjohnegan@hotmail.co.uk>
2019-01-10 21:54:32 +0000

    Enabled integration tests in Maven
    * Renamed existing test to be integration test
    * Added rest-assured and actuator dependencies

--- src/test/java/uk/co/hotmail/connorjohnegan/springbootresttdd/SpringBootRestTddApplicationIT.java
+++ src/test/java/uk/co/hotmail/connorjohnegan/springbootresttdd/SpringBootRestTddApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootRestTddApplicationTests {
+public class SpringBootRestTddApplicationIT {
 
     @Test
     public void contextLoads() {
