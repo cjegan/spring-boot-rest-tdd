b381e352190d2dd2c710ab6b989462d3ffed206f
Connor Egan <connorjohnegan@hotmail.co.uk>
2019-01-10 21:35:22 +0000

    Initialised project using Spring Initialiser

---
+++ src/main/java/uk/co/hotmail/connorjohnegan/springbootresttdd/SpringBootRestTddApplication.java
@@ -0,0 +1,14 @@
+package uk.co.hotmail.connorjohnegan.springbootresttdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestTddApplication.class, args);
+    }
+
+}
+
